f5ee578ba44859fa50e7812b71c1d7e2afb4d25f
sudomonikers <sudomonikers@gmail.com>
2023-07-21 13:20:54 -0700

    get files from s3

--- backend/api/helper_functions/get_file_from_s3.py
+++ backend/api/helper_functions/get_file_from_s3.py
@@ -5,6 +5,7 @@ ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
 SECRET_KEY = os.getenv('AWS_SECRET_KEY')
 
 
-def upload_file_to_aws(local_file: str, bucket: str, s3_file: str):
+def download_file_from_s3(bucket_name: str, s3_file_key: str, local_file_path: str):
     s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
-    s3.upload_file(local_file, bucket, s3_file)
+    s3.download_file(Bucket=bucket_name, Key=s3_file_key, Filename=local_file_path)
+
--- backend/api/router.py
+++ backend/api/router.py
@@ -1,13 +1,15 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException
+from fastapi import APIRouter, File, UploadFile, HTTPException, Response
 import textract
 import os
 from nltk.tokenize import sent_tokenize
 from pydantic import BaseModel
+from typing import Optional
 
 from helper_functions.save_file_to_disk import save_file_to_disk
 from helper_functions.create_embeddings import get_vector_embeddings
 from helper_functions.upsert_vector_to_db import upsert_vectors, index
 from helper_functions.upload_to_s3 import upload_file_to_aws
+from helper_functions.get_file_from_s3 import download_file_from_s3
 
 router = APIRouter()
 
@@ -52,5 +54,17 @@ async def chat(message: ChatMessage):
 
 
 @router.get("/document/{doc_title}")
-async def get_document(doc_title: str):
-    return {"doc_title": doc_title}
+async def get_document(doc_title: str) -> Optional[bytes]:
+    try: 
+        download_file_from_s3(os.getenv("S3_BUCKET"), doc_title, f'temp_files/{doc_title}')
+        file_contents = open(f'temp_files/{doc_title}', 'rb').read()
+        
+        file_extension = os.path.splitext(doc_title)[-1].lower()
+        media_type = f"application/{file_extension[1:]}"
+        
+        response = Response(content=file_contents, media_type=media_type)
+        response.headers["Content-Disposition"] = f"attachment; filename={doc_title.split('/')[-1]}"
+        os.remove(f'temp_files/{doc_title}')  # Clean up the temporary file
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {e}")
